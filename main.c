67d067bbbefe5d826ba2f280ae2d34c088192c18
aav <asaph.avanet@esme.fr>
2024-09-26 17:00:36 +0200

    update welcome message

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
 #include <stdio>[D.h>
-int main() {}[Dprintln("Hello world")[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Df[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C; 
+int main() {}[Dprintln("Hello world edited")[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Df[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C; 
 return 0;
 }
